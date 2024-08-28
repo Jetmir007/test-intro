9b0f5cef0a852cc01fdd7c45685125695f6f8b7f
Jetmir <jeku07001@utb.vaxjo.se>
2024-08-28 11:16:51 +0200

    Uppgift 1 prog

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write two numbers");
+string text1 = Console.ReadLine();
+string text2 = Console.ReadLine();
+int tal = int.Parse(text1);
+int tal2 = int.Parse(text2);
+Console.WriteLine(tal + tal2);
+Console.WriteLine(tal - tal2);
+
+
+/*Console.WriteLine("Vad är ditt förnamn och efternamn");
+string text1 = Console.ReadLine();
+string text2 = Console.ReadLine();
+Console.WriteLine(text1 + " " + text2);
+*/
